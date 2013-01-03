e0d0c9d7633ba3ed6e924888e25f3bb75c8b7d02
Michael Arnold <github@razorsedge.org>
2013-01-02 23:22:14 -0700

    Three more attempts at fixing Travis-CI failures.
    Revert Two more attempts at fixing Travis-CI failures.
    Collapse before :each in context 'with no ethernet interfaces'.
    Lets try "eq()" instead of "==".

--- spec/unit/facter/openlldp_spec.rb
+++ spec/unit/facter/openlldp_spec.rb
@@ -34,11 +34,8 @@ describe 'openlldp facts' do
     end
 
     context 'with no ethernet interfaces' do
-      before :each do
-        Facter.fact(:interfaces).stubs(:value).returns('lo')
-      end
-
       it 'no lldp_neighbor_* facts should be available' do
+        Facter.fact(:interfaces).stubs(:value).returns('lo')
         # I have no idea what I am doing here. How do I test for code that does not run?
         proc { Facter.value(:lldp_neighbor_chassisid_em2) }.should_not raise_error
         Facter.value(:lldp_neighbor_chassisid_em2).should be_nil
@@ -51,37 +48,6 @@ describe 'openlldp facts' do
       it 'lldp_neighbor_chassisid_em2 is nil' do
         Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 1 2>/dev/null').returns(my_fixture_read('device_not_found'))
         Facter.value(:lldp_neighbor_chassisid_em2).should be_nil
-      end
-    end
-
-    context 'with LLDP neighbor' do
-      it 'lldp_neighbor_chassisid_em2 is 00:21:86:9f:89:17' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 1 2>/dev/null').returns(my_fixture_read('chassisID'))
-        Facter.value(:lldp_neighbor_chassisid_em2).should == '00:21:86:9f:89:17'
-      end
-      it 'lldp_neighbor_portid_em2 is Ethernet40' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 2 2>/dev/null').returns(my_fixture_read('portID'))
-        Facter.value(:lldp_neighbor_portid_em2).should == 'Ethernet40'
-      end
-      it 'lldp_neighbor_sysname_em2 is motivation.localdomain' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 5 2>/dev/null').returns(my_fixture_read('sysName'))
-        Facter.value(:lldp_neighbor_sysname_em2).should == 'motivation.localdomain'
-      end
-      it 'lldp_neighbor_mngaddr_ipv4_em2 is 192.168.2.57' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 8 2>/dev/null').returns(my_fixture_read('mngAddr'))
-        Facter.value(:lldp_neighbor_mngaddr_ipv4_em2).should == '192.168.2.57'
-      end
-      it 'lldp_neighbor_mngaddr_ipv6_em2 is ::192.168.2.57' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 8 2>/dev/null').returns(my_fixture_read('mngAddr'))
-        Facter.value(:lldp_neighbor_mngaddr_ipv6_em2).should == '::192.168.2.57'
-      end
-      it 'lldp_neighbor_pvid_em2 is 1' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 0x0080c201 2>/dev/null').returns(my_fixture_read('PVID'))
-        Facter.value(:lldp_neighbor_pvid_em2).should == 1
-      end
-      it 'lldp_neighbor_mtu_em2 is 9236' do
-        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 0x00120f04 2>/dev/null').returns(my_fixture_read('MTU'))
-        Facter.value(:lldp_neighbor_mtu_em2).should == '9236'
       end
     end
 
@@ -113,6 +79,44 @@ describe 'openlldp facts' do
       it 'lldp_neighbor_mtu_em2 is nil' do
         Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 0x00120f04 2>/dev/null').returns(nil)
         Facter.value(:lldp_neighbor_mtu_em2).should be_nil
+      end
+    end
+
+    context 'with LLDP neighbor' do
+      it 'lldp_neighbor_chassisid_em2 is 00:21:86:9f:89:17' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 1 2>/dev/null').returns(my_fixture_read('chassisID'))
+        #Facter.value(:lldp_neighbor_chassisid_em2).should == '00:21:86:9f:89:17'
+        Facter.value(:lldp_neighbor_chassisid_em2).should eq('00:21:86:9f:89:17')
+      end
+      it 'lldp_neighbor_portid_em2 is Ethernet40' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 2 2>/dev/null').returns(my_fixture_read('portID'))
+        #Facter.value(:lldp_neighbor_portid_em2).should == 'Ethernet40'
+        Facter.value(:lldp_neighbor_portid_em2).should eq('Ethernet40')
+      end
+      it 'lldp_neighbor_sysname_em2 is motivation.localdomain' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 5 2>/dev/null').returns(my_fixture_read('sysName'))
+        #Facter.value(:lldp_neighbor_sysname_em2).should == 'motivation.localdomain'
+        Facter.value(:lldp_neighbor_sysname_em2).should eq('motivation.localdomain')
+      end
+      it 'lldp_neighbor_mngaddr_ipv4_em2 is 192.168.2.57' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 8 2>/dev/null').returns(my_fixture_read('mngAddr'))
+        #Facter.value(:lldp_neighbor_mngaddr_ipv4_em2).should == '192.168.2.57'
+        Facter.value(:lldp_neighbor_mngaddr_ipv4_em2).should eq('192.168.2.57')
+      end
+      it 'lldp_neighbor_mngaddr_ipv6_em2 is ::192.168.2.57' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 8 2>/dev/null').returns(my_fixture_read('mngAddr'))
+        #Facter.value(:lldp_neighbor_mngaddr_ipv6_em2).should == '::192.168.2.57'
+        Facter.value(:lldp_neighbor_mngaddr_ipv6_em2).should eq('::192.168.2.57')
+      end
+      it 'lldp_neighbor_pvid_em2 is 1' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 0x0080c201 2>/dev/null').returns(my_fixture_read('PVID'))
+        #Facter.value(:lldp_neighbor_pvid_em2).should == 1
+        Facter.value(:lldp_neighbor_pvid_em2).should eq(1)
+      end
+      it 'lldp_neighbor_mtu_em2 is 9236' do
+        Facter::Util::Resolution.stubs(:exec).with('lldptool get-tlv -n -i em2 -V 0x00120f04 2>/dev/null').returns(my_fixture_read('MTU'))
+        #Facter.value(:lldp_neighbor_mtu_em2).should == '9236'
+        Facter.value(:lldp_neighbor_mtu_em2).should eq('9236')
       end
     end
   end
